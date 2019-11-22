6b09898cfb4482070d10f7159a447281363a1765
Dan Gyi <daniel.gyi@gmail.com>
2019-11-22 13:11:51 +0000

    update upload method

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -15,37 +15,118 @@ class Form extends React.Component {
 }
 
 class Upload extends React.Component {
+  onFilesAdded(files) {
+    this.setState(prevState => ({
+      files: prevState.files.concat(files)
+    }));
+  }
+  
   constructor(props) {
     super(props)
-  }
+    this.state = {
+      files: [],
+      uploading: false,
+      uploadProgress: {},
+      successfullUploaded: false
+    };
+    this.onFilesAdded = this.onFilesAdded.bind(this);
+    this.uploadFiles = this.uploadFiles.bind(this);
+    this.sendRequest = this.sendRequest.bind(this);
+    this.renderActions = this.renderActions.bind(this);
+  };
   render() {
-    return ( 
-    <div className="Upload">
-      <span className="Title">Upload Your Cat or Dog Photo</span>
-      <div className="Content">
-        <div />
-        <div className="Files" />
+    return (
+      <div className="Upload">
+        <span className="Title">Upload Your Cat or Dog Photo</span>
+        <div className="Content">
+          <div>
+            <Dropzone
+              onFilesAdded={this.onFilesAdded}
+              disabled={this.state.uploading || this.state.successfullUploaded}
+            />
+          </div>
+          <div className="Files">
+            {this.state.files.map(file => {
+              return (
+                <div key={file.name} className="Row">
+                  <span className="Filename">{file.name}</span>
+                  {this.renderProgress(file)}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="Actions">{this.renderActions()}</div>
       </div>
-      <div className="Actions" />
-    </div>
-    )
-  }
-}
+    );
+  };
+};
 
 class Dropzone extends React.Component {
   openFileDialog() {
     if (this.props.disabled) return;
     this.fileInputRef.current.click();
   };
+
+  onFilesAdded(e) {
+    if (this.props.disabled) return;
+      const files = e.target.files;
+    if (this.props.onFilesAdded) {
+      const array = this.fileListToArray(files);
+      this.props.onFilesAdded(array);
+    }
+  };
+
+  onDragOver(evt) {
+    evt.preventDefault();
+    if (this.props.disabled) return;
+    this.setState({ highlight: true });
+  }
+
+  onDragLeave() {
+    this.setState({ highlight: false });
+  }
+
+  onDrop(event) {
+    event.preventDefault();
+    if (this.props.disabled) return;
+    const files = event.dataTransfer.files;
+    if (this.props.onFilesAdded) {
+      const array = this.fileListToArray(files);
+      this.props.onFilesAdded(array);
+    }
+    this.setState({ highlight: false });
+  }
+
+  fileListToArray(list) {
+    const array = [];
+    for (var i = 0; i < list.length; i++) {
+      array.push(list.item(i));
+    }
+    return array;
+  };
+
   constructor(props){ 
     super(props)
+    this.state = { highlight: false };
     this.fileInputRef = React.createRef()
-    
+    this.openFileDialog = this.openFileDialog.bind(this);
+    this.onFilesAdded = this.onFilesAdded.bind(this);
+    this.onDragOver = this.onDragOver.bind(this);
+    this.onDragLeave = this.onDragLeave.bind(this);
+    this.onDrop = this.onDrop.bind(this);
   };
+  
 
   render() {
     return(
-      <div className="Dropzone">
+      <div className={`Dropzone ${this.state.highlight ? "Highlight" : ""}`}
+        onClick={this.openFileDialog}
+        onDragOver={this.onDragOver}
+        onDragLeave={this.onDragLeave}
+        onDrop={this.onDrop}
+        style={{ cursor: this.props.disabled ? "default" : "pointer" }}
+      >
         <img
           alt="upload"
           className="Icon"
